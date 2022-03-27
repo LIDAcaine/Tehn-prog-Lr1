643b92e6c5f8e37db3a8b4a68ddb59315aa842bf
LIDAcaine <99477134+LIDAcaine@users.noreply.github.com>
2022-03-27 17:24:17 +0300

    Create queue_1.cpp

---
+++ queue_1.cpp
@@ -0,0 +1,25 @@
+#include "queue_1.h"
+int Queue_1::math_operation()
+{
+    int size = Queue_Size();
+    if (size < 2)
+    {
+        return -1;
+
+    }
+    int* arr = new int[size];
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        arr[i] = remove();
+        this->insert(arr[i]);
+    }
+    int count = 0;
+    for (i = 0; i < size; i++) {
+        if (arr[i] < arr[i + 1]) {
+            count++;
+        }
+        
+    }
+    delete[] arr;
+    return count;
+}
